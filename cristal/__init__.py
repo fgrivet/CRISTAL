a889e9369b6cb760a5d2ca9861191252255984eb
fgrivet <floriangrivet.pro@gmail.com>
2026-07-02 12:00:45 +0200

    Add: License, copyright, startup notice

--- cristal/__init__.py
+++ cristal/__init__.py
@@ -1,7 +1,37 @@
-from .__version__ import __author__, __date__, __version__
+"""
+CRISTAL - A Python anomaly detection framework based on the Christoffel Function.
+Copyright (C) 2026 Florian Grivet
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import logging
+
+from .__version__ import __author__, __date__, __license__, __version__
 from .backend import *
 from .commons import *
 from .config import *
 from .core import *
 from .evaluation import *
 from .preprocessing import *
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+logger.info(f"CRISTAL v{__version__} (released: {__date__})\nCopyright (C) 2026 Florian Grivet")
--- cristal/__version__.py
+++ cristal/__version__.py
@@ -1,5 +1,6 @@
 """Contains the informations on the version of the CRISTAL package."""
 
-__version__ = "0.0.1"  #: The current version of the CRISTAL package
-__date__ = "2026-03-04"  #: The release date of the CRISTAL package
+__version__ = "0.0.2"  #: The current version of the CRISTAL package
+__date__ = "2026-07-02"  #: The release date of the CRISTAL package
 __author__ = "fgrivet"  #: The author of the CRISTAL package
+__license__ = "GPL-3.0"  #: The license of the CRISTAL package
