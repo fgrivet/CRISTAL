5159c4618c66d645ace33482b7a93ac3cbf11b92
fgrivet <floriangrivet.pro@gmail.com>
2026-07-17 15:30:55 +0200

    Fix: test_polynomial_basis to account for rounding errors

--- tests/commons/test_polynomial_basis.py
+++ tests/commons/test_polynomial_basis.py
@@ -105,11 +105,11 @@ class TestPolynomialBasis(unittest.TestCase):
         # Without normalization
         result_mon = mon_basis.vandermonde_1d(X, n, 5.0, normalize=False)
         desired_result_mon = self.vandermonde_1d(X, n, self.monomials)
-        np.testing.assert_equal(result_mon, desired_result_mon, "Monomials basis")
+        np.testing.assert_allclose(result_mon, desired_result_mon, err_msg="Monomials basis")
 
         # With normalization (by default None so True)
         result_mon = mon_basis.vandermonde_1d(X, n, 5.0)  # Same result because normalization is only for Chebyshev basis
-        np.testing.assert_equal(result_mon, desired_result_mon, "Monomials basis")
+        np.testing.assert_allclose(result_mon, desired_result_mon, err_msg="Monomials basis")
 
         # Test chebyshev basis
         cheb_basis = PolynomialBasis(basis="chebyshev")
@@ -118,12 +118,12 @@ class TestPolynomialBasis(unittest.TestCase):
         # Without normalization
         result_cheb = cheb_basis.vandermonde_1d(X, n, 5.0, normalize=False)
         desired_result_cheb = self.vandermonde_1d(X, n, self.chebyshev)
-        np.testing.assert_almost_equal(result_cheb, desired_result_cheb, 12, "Chebyshev basis")
+        np.testing.assert_allclose(result_cheb, desired_result_cheb, 12, err_msg="Chebyshev basis")
 
         # With normalization (by default None so True)
         result_cheb = cheb_basis.vandermonde_1d(X, n, 2.0)
         desired_result_cheb = self.vandermonde_1d(X / 4 - 1, n, self.chebyshev)
-        np.testing.assert_almost_equal(result_cheb, desired_result_cheb, 12, "Chebyshev basis normalized")
+        np.testing.assert_allclose(result_cheb, desired_result_cheb, 12, err_msg="Chebyshev basis normalized")
 
         # Test invalid values
         # Wrong X shape
@@ -151,7 +151,7 @@ class TestPolynomialBasis(unittest.TestCase):
 
         result_mon = mon_basis.vandermonde_nd(X, n)
         desired_result_mon = self.vandermonde_nd(X, n, self.monomials, comb)
-        np.testing.assert_equal(result_mon, desired_result_mon, "Monomials basis")
+        np.testing.assert_allclose(result_mon, desired_result_mon, err_msg="Monomials basis")
 
         # Test chebyshev basis
         cheb_basis = PolynomialBasis(basis="chebyshev")
@@ -159,7 +159,7 @@ class TestPolynomialBasis(unittest.TestCase):
 
         result_cheb = cheb_basis.vandermonde_nd(X, n)
         desired_result_cheb = self.vandermonde_nd(X, n, self.chebyshev, comb)
-        np.testing.assert_almost_equal(result_cheb, desired_result_cheb, 12, "Chebyshev basis")
+        np.testing.assert_allclose(result_cheb, desired_result_cheb, 12, err_msg="Chebyshev basis")
 
         # Test invalid values
         # Wrong X shape
